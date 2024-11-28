9ee97744ccc8799bfeedf58515a208ed779b748d
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-28 17:01:34 -0600

    Updated e2e 004

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,3 +1,5 @@
+import { getQuestions } from "../../client/src/services/questionApi";
+
 describe('Tech Quiz Application - E2E', () => {
     beforeEach(() => {
       // Intercept the API request to ensure consistent results
@@ -33,30 +35,40 @@ describe('Tech Quiz Application - E2E', () => {
       // Start the quiz
       cy.get('button').contains('Start Quiz').click();
     //   cy.wait('@getQuestions');
-    cy.wait('@getQuestions').then((interception) => {
-        expect(interception.response).to.exist;
+    // cy.wait('@getQuestions').then((interception) => {
+    //     if(interception.response){
+    //     cy.log('Intercepted Response:', interception.response?.body);
+    //     }else{
+    //       throw new Error('API response is undefined');
+    //     }
+    //   });
+  
+    cy.wait('@getQuestions').its('response.statusCode').should('eq', 200);
+    // cy.wait('@getQuestions').then((interception) => {
+    //     expect(interception.response).to.exist;
         
-        if (interception.response) {
-            console.log('API Response:', interception.response.body);
-          expect(interception.response.body).to.exist;
-          expect(interception.response.body[0].answers[0].text).to.eq('A library');
-        }else {
-            throw new Error('API response is undefined');
-          }
+    //     if (interception.response) {
+    //         console.log('API Response:', interception.response.body);
+    //       expect(interception.response.body).to.exist;
+    //       expect(interception.response.body[0].answers[0].text).to.eq('A library');
+    //     }else {
+    //         throw new Error('API response is undefined');
+    //       }
         
-      });
+    //   });
     //   cy.get('.btn-primary').then(($btns) => {
     //     $btns.each((index, btn) => {
     //       console.log(`Button ${index}: ${btn.textContent}`);
     //     });
     //   });
     cy.get('h2').should('contain', 'What is React?');
-    cy.get('.btn-primary').should('have.length', 4);
-    cy.get('.btn-primary').each(($btn, index) => {
-        console.log(`Button ${index}: ${$btn.text()}`);
-      });
-    cy.get('.btn-primary').contains('A library', { timeout: 15000 }).should('be.visible').click();
-  
+    cy.get('.btn-primary').should('have.length', 4).contains('A library').should('exist').click();
+
+    // Wait for the button to become visible and interact with it
+    // cy.get('.btn-primary')
+    // .filter((_, el) => el.textContent?.trim() === 'A library')
+    // .should('be.visible')
+    // .click();
         // Verify the second question
       cy.get('h2').should('contain', 'What is Node.js?');
       cy.get('.btn-primary').contains('A runtime').click();
