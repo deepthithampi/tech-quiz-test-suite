d8a69a7b22d6c65e573d94ec9ccbbc11d8c11c36
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-28 13:32:22 -0600

    Updated e2e 002

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -32,8 +32,26 @@ describe('Tech Quiz Application - E2E', () => {
   
       // Start the quiz
       cy.get('button').contains('Start Quiz').click();
-      cy.wait('@getQuestions');
-  
+    //   cy.wait('@getQuestions');
+    cy.wait('@getQuestions').then((interception) => {
+        expect(interception.response).to.exist;
+        if (interception.response) {
+          expect(interception.response.body).to.exist;
+          expect(interception.response.body[0].answers[0].text).to.eq('A library');
+        }
+        
+      });
+      cy.get('.btn-primary').then(($btns) => {
+        $btns.each((index, btn) => {
+          console.log(`Button ${index}: ${btn.textContent}`);
+        });
+      });
+      cy.get('h2').should('exist');
+      cy.get('.spinner-border').should('not.exist'); 
+      cy.get('h2').should('contain', 'What is React?');
+      cy.get('.btn-primary').should('have.length', 4);
+      cy.get('.btn-primary').debug();
+      cy.get('.btn-primary').contains('A library').should('exist'); 
       // Answer the first question
       cy.get('.btn-primary').contains('A library').click();
       cy.get('h2').should('contain', 'What is Node.js?');
